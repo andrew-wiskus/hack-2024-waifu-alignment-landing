666b2a22d2a56bfa7179fc604f07ab179da9337c
andrew <andrewwiskus@gmail.com>
2024-11-09 16:50:57 -0300

    padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-dvw h-dvh flex flex-col items-center justify-center bg-white">
+    <div className="w-dvw h-dvh flex flex-col items-center justify-center bg-white padding-20">
       <a href="https://www.loom.com/share/94dcb57ec05e42c5aa3e37ff95a09116?sid=d1e6ba83-1365-47db-803a-b2e303d40236">
         <img src="/banner.png" alt="waifus for alignment" className="border-[20px] border-black rounded-md object-contain object-bottom" />
       </a>
