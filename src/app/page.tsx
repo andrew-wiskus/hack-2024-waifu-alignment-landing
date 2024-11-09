77d0f7b4c30146f65eeb4038d1f3c5668499b277
andrew <andrewwiskus@gmail.com>
2024-11-09 17:06:16 -0300

    demo video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
 
 export default function Home() {
+  const [clicked, setClicked] = useState(false)
   return (
-    <div className="w-dvw h-dvh flex flex-col items-center justify-center bg-white padding-20">
-      <a href="https://www.loom.com/share/94dcb57ec05e42c5aa3e37ff95a09116?sid=d1e6ba83-1365-47db-803a-b2e303d40236">
-        <img src="/banner.png" alt="waifus for alignment" className="border-[20px] border-black rounded-md object-contain object-bottom" />
-      </a>
-      <a href="https://www.loom.com/share/94dcb57ec05e42c5aa3e37ff95a09116?sid=d1e6ba83-1365-47db-803a-b2e303d40236">
-        <p className="text-md text-center underline text-gray-800 mt-4">CLICK ME</p>
-      </a>
+    <div className="w-dvw h-dvh flex flex-col items-center justify-center bg-white p-20">
+      {clicked ?
+        <iframe src="https://www.loom.com/embed/94dcb57ec05e42c5aa3e37ff95a09116?sid=0d2a346e-8b36-41de-a0b6-adacaac7d5b6" allowFullScreen className="absolute top-0 left-0 w-full h-full"></iframe>
+        :
+        <>
+          <div className='cursor-pointer' onClick={() => setClicked(true)}>
+            <img src="/banner.png" alt="waifus for alignment" className="border-[20px] border-black rounded-md object-contain object-bottom" />
+          </div>
+          <div className='cursor-pointer' onClick={() => setClicked(true)}>
+            <p className="text-md text-center underline text-gray-800 mt-4">CLICK ME</p>
+          </div>
+        </>
+      }
     </div>
   );
 }
